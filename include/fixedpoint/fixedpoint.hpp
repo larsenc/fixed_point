3ea143e2f7f8e8ea6d018e5be78c0541817b0264
larsenc <larsenc@student.chalmers.se>
2021-02-21 18:24:24 +0100

    Simplify and move multiplication operator outside class

--- include/fixedpoint/fixedpoint.hpp
+++ include/fixedpoint/fixedpoint.hpp
@@ -71,15 +71,22 @@ namespace fixed_point {
 		template<uint8_t M_OLD, uint8_t N_OLD, uint8_t M_NEW, uint8_t N_NEW>
 		struct converter<M_OLD, N_OLD, M_NEW, N_NEW, true>
 		{
+			typedef typename detail::storage<M_OLD + N_OLD>::type old_storage_type;
 			typedef typename detail::storage<M_NEW + N_NEW>::type new_storage_type;
 
 			// For the case N_OLD <= N_NEW we need to shift left.
-			static scaled_int<M_NEW, N_NEW> convert(const scaled_int<M_OLD, N_OLD>& old)
+			static scaled_int<M_NEW, N_NEW> convert(const scaled_int<M_OLD, N_OLD>& oldType)
 			{
 				detail::static_assert_<N_OLD <= N_NEW>();
-				new_storage_type newValue = old.getValue();
+				return scaled_int<M_NEW, N_NEW>(convert(oldType.getValue()));
+			}
+
+			static new_storage_type convert(const old_storage_type& oldValue)
+			{
+				detail::static_assert_<N_OLD <= N_NEW>();
+				new_storage_type newValue = oldValue;
 				newValue <<= -(N_OLD - N_NEW);
-				return scaled_int<M_NEW, N_NEW>(newValue);
+				return newValue;
 			}
 		};
 
@@ -90,25 +97,29 @@ namespace fixed_point {
 			typedef typename detail::storage<M_NEW + N_NEW>::type new_storage_type;
 
 			// For the case N_OLD > N_NEW we need to shift right and round
-			static scaled_int<M_NEW, N_NEW> convert(const scaled_int<M_OLD, N_OLD>& old)
+			static scaled_int<M_NEW, N_NEW> convert(const scaled_int<M_OLD, N_OLD>& oldType)
+			{
+				detail::static_assert_<(N_OLD > N_NEW)>();
+				return scaled_int<M_NEW, N_NEW>(convert(oldType.getValue()));
+			}
+
+			static new_storage_type convert(old_storage_type value)
 			{
 				detail::static_assert_<(N_OLD > N_NEW)>();
 				// Since we are decreasing the resolution, we need to round the old value.
 				// We therefore add half the resolution of the old value before shifting right
-				old_storage_type newValue = old.getValue();
 
 				// Right shift of signed ints are compiler specific. I therefore want to use / instead of right shift.
 				// TODO: Determine if there is a more efficient way when using /
-				if (newValue < 0) {
-					newValue -= (1 << (N_OLD - N_NEW - 1));
+				if (value < 0) {
+					value -= (1 << (N_OLD - N_NEW - 1));
 				}
 				else {
-					newValue += (1 << (N_OLD - N_NEW - 1));
-
+					value += (1 << (N_OLD - N_NEW - 1));
 				}
-				newValue = newValue / (1 << (N_OLD - N_NEW));
+				value = value / (1 << (N_OLD - N_NEW));
 
-				return scaled_int<M_NEW, N_NEW>(static_cast<new_storage_type>(newValue));
+				return static_cast<new_storage_type>(value);
 			}
 		};
 
@@ -332,6 +343,13 @@ namespace fixed_point {
 	{
 		const typename scaled_int<M, N>::intermediate_type intermediate = lhs.getValue() / rhs.getValue();
 		return scaled_int<M, N>(intermediate << N);
+	}
+
+	template<uint8_t M, uint8_t N>
+	scaled_int<M, N> operator*(const scaled_int<M, N>& lhs, const scaled_int<M, N>& rhs)
+	{
+		const typename scaled_int<M, N>::intermediate_type intermediate = lhs.getValue() * rhs.getValue();
+		return scaled_int<M, N>(detail::converter<2 * M + 1, 2 * N, M, N, (2 * N <= N)>::convert(intermediate));
 	}
 
 	typedef scaled_int<3, 4>	scaled_int_3_4_t;
